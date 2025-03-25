750cc65f26e7d4da3c8a480510867076dfa7b2ed
prasetion <prasetion@gmail.com>
2025-03-25 10:29:40 +0700

    add questions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import logoQuiz from "../assets/quiz-logo.png";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <img src={logoQuiz} alt="logo quiz" />
+      <h1>React Quiz </h1>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  const handleSelectButton = (selectedAnswer) => {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  };
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectButton(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;

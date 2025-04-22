48270107f3a4ddb89c5e9778fe66a20ad1ab69ea
prasetion <prasetion@gmail.com>
2025-04-22 09:15:53 +0700

    question timer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -8,6 +8,11 @@ const Question = ({ index, onSelectAnswer, onSkipAnswer }) => {
     isCorrect: null,
   });
 
+  let timer = 10000;
+  if (answer.selectedAnswer) timer = 1000;
+
+  if (answer.isCorrect !== null) timer = 2000;
+
   const handleSelectAnswer = (answer) => {
     setAnswer({
       selectedAnswer: answer,
@@ -36,7 +41,12 @@ const Question = ({ index, onSelectAnswer, onSkipAnswer }) => {
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[index].text}</h2>
       <Answers
         answers={QUESTIONS[index].answers}
--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const QuestionTimer = ({ timeout, onTimeout }) => {
+const QuestionTimer = ({ timeout, onTimeout, mode }) => {
   const [remainingTime, setRemainingTime] = useState(timeout);
 
   useEffect(() => {
@@ -18,7 +18,12 @@ const QuestionTimer = ({ timeout, onTimeout }) => {
   }, []);
 
   return (
-    <progress id="question-time" max={timeout} value={remainingTime}>
+    <progress
+      id="question-time"
+      max={timeout}
+      value={remainingTime}
+      className={mode}
+    >
       QuestionTimer
     </progress>
   );
